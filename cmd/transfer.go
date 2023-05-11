777461f3b9ef268841ddbad2e6cb4efbb109dc5b
Dongri Jin <dongri.jin@speee.jp>
2023-05-12 07:30:46 +0900

    Replace timeout to timeoutHeight
    Signed-off-by: Dongri Jin <dongri.jin@speee.jp>

--- cmd/transfer.go
+++ cmd/transfer.go
@@ -20,13 +20,13 @@ func transferCmd() *cobra.Command {
 	var tokenAddress string
 	var portID string
 	var channelID string
-	var timeout uint64
+	var timeoutHeight uint64
 	cmd := &cobra.Command{
 		Use:   "transfer",
 		Short: "transfer token from one account to another chain's wallet",
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
 			ctx := cmd.Context()
-			if err := Transfer(ctx, rpcAddress, chainID, mnemonic, ics20BankAddress, ics20TransferBankAddress, uint32(fromIndex), toAddress, amount, tokenAddress, portID, channelID, timeout); err != nil {
+			if err := Transfer(ctx, rpcAddress, chainID, mnemonic, ics20BankAddress, ics20TransferBankAddress, uint32(fromIndex), toAddress, amount, tokenAddress, portID, channelID, timeoutHeight); err != nil {
 				return err
 			}
 			return nil
@@ -43,12 +43,12 @@ func transferCmd() *cobra.Command {
 	cmd.Flags().StringVar(&tokenAddress, "token-address", "", "address of the token contract")
 	cmd.Flags().StringVar(&portID, "port-id", "", "port id")
 	cmd.Flags().StringVar(&channelID, "channel-id", "", "channel id")
-	cmd.Flags().Uint64Var(&timeout, "timeout", 0, "timeout")
+	cmd.Flags().Uint64Var(&timeoutHeight, "timeout-height", 0, "timeout height")
 
 	return cmd
 }
 
-func Transfer(ctx context.Context, rpcAddress string, chainID int64, mnemonic, ics20BankAddress, ics20TransferBankAddress string, fromIndex uint32, toAddress string, amount int64, tokenAddress, portID, channelID string, timeout uint64) error {
+func Transfer(ctx context.Context, rpcAddress string, chainID int64, mnemonic, ics20BankAddress, ics20TransferBankAddress string, fromIndex uint32, toAddress string, amount int64, tokenAddress, portID, channelID string, timeoutHeight uint64) error {
 	chainA, err := geth.InitializeChain(rpcAddress, chainID, mnemonic, tokenAddress, ics20TransferBankAddress, ics20BankAddress)
 	if err != nil {
 		return err
@@ -81,7 +81,7 @@ func Transfer(ctx context.Context, rpcAddress string, chainID int64, mnemonic, i
 		uint64(amount),
 		common.HexToAddress(toAddress),
 		portID, channelID,
-		timeout+1000,
+		timeoutHeight,
 	)
 	if err != nil {
 		return err
